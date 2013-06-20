9097bf12cd6e5b2582e22e09ca49f7c2536d7da9
isxxguo <isxxguo@pptv.com>
2013-06-20 02:26:30 +0000

    /base/branches/1.2.2 /build/android 增加 android 测试项目 emailto: box-dev
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@6970 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ src/com/pplive/demo/CameraPlayer.java
@@ -0,0 +1,50 @@
+package com.pplive.demo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.widget.Toast;
+import android.webkit.WebViewClient;
+
+public class CameraPlayer extends WebViewClient {
+
+	private Activity owner;
+	
+	public CameraPlayer(Activity owner)
+	{
+		this.owner = owner;
+	}
+	
+	@Override
+	public void onReceivedError(WebView view, int errorCode, String description, String failingUrl)
+	{
+		capture(null);
+	}
+	
+	@JavascriptInterface
+	public void capture(final String url)
+	{
+		owner.runOnUiThread(new Runnable() {
+			@Override
+			public void run()
+			{
+		        try
+		        {
+		            Intent intent = new Intent(
+		            		owner.getBaseContext(), CameraActivity.class);
+		            Bundle bl = new Bundle();
+					bl.putString("url", url);
+					intent.putExtras(bl);
+		            owner.startActivityForResult(intent, 0);
+		        }
+		        catch (Exception e)
+		        {
+		            Toast.makeText(owner, "Open Failed", Toast.LENGTH_SHORT).show();
+		        }
+			}
+		});
+	}
+
+}
